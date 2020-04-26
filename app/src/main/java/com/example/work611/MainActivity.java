710e2f7a1e29c70eb567a60beedb2fdc14a1fe51
Yarchik <yarchike@gmail.com>
2020-04-26 20:45:52 +0300

    Убраны лишние инцилизации textViev, передалано логированик

--- app/src/main/java/com/example/work611/MainActivity.java
+++ app/src/main/java/com/example/work611/MainActivity.java
@@ -12,85 +12,77 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Lifecycle";
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName() + ": " + savedInstanceState);
-        TextView textView = findViewById(R.id.textViev);
-        textView.append("\n" + new Object(){}.getClass().getEnclosingMethod().getName() + ": " + savedInstanceState);
+        textView = findViewById(R.id.textViev);
+        Log.d(TAG, "onCreate" + ": " + savedInstanceState);
+        textView.append("\n" + "onCreate" + ": " + savedInstanceState);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
-        textView.append("\n" + new Object(){}.getClass().getEnclosingMethod().getName());
+        Log.d(TAG, "onStart");
+        textView.append("\n" + "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
-        textView.append("\n" + new Object(){}.getClass().getEnclosingMethod().getName());
+        Log.d(TAG, "onResume");
+        textView.append("\n" + "onResume");
     }
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
-        textView.append("\n" + new Object(){}.getClass().getEnclosingMethod().getName());
+        Log.d(TAG, "onPostResume");
+        textView.append("\n" + "onPostResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
-        textView.append("\n" + new Object(){}.getClass().getEnclosingMethod().getName());
+        Log.d(TAG, "onPause");
+        textView.append("\n" + "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
-        textView.append("\n" + new Object(){}.getClass().getEnclosingMethod().getName());
+        Log.d(TAG, "onStop");
+        textView.append("\n" + "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
-        textView.append("\n" + new Object(){}.getClass().getEnclosingMethod().getName());
+        Log.d(TAG, "onDestroy");
+        textView.append("\n" + "onDestroy");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
-        textView.append("\n" + new Object(){}.getClass().getEnclosingMethod().getName());
+        Log.d(TAG, "onRestart");
+        textView.append("\n" + "onRestart");
     }
 
     @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
-        textView.append("\n" + new Object(){}.getClass().getEnclosingMethod().getName());
+        Log.d(TAG, "onPostCreate");
+        textView.append("\n" + "onPostCreate");
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
-        textView.append("\n" + new Object(){}.getClass().getEnclosingMethod().getName());
+        Log.d(TAG, "onKeyDown");
+        textView.append("\n" + "onKeyDown");
 
         return true;//super.onKeyDown(keyCode, event);
 
@@ -98,29 +90,29 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onKeyLongPress(int keyCode, KeyEvent event) {
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
+        Log.d(TAG, "onKeyLongPress");
+        textView.append("\n" + "onKeyLongPress");
         return super.onKeyLongPress(keyCode, event);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
+        Log.d(TAG, "onBackPressed");
+        textView.append("\n" + "onBackPressed");
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
+        Log.d(TAG, "onSaveInstanceState");
+        textView.append("\n" + "onSaveInstanceState");
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        Log.d(TAG, new Object(){}.getClass().getEnclosingMethod().getName());
-        TextView textView = findViewById(R.id.textViev);
+        Log.d(TAG, "onRestoreInstanceState");
+        textView.append("\n" + "onRestoreInstanceState");
     }
 }
